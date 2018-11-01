3c305e0bbdd65ebed34b54a9f24e8fecda4bddac
Renato Araujo <orenatoaraujo@hotmail.com>
2018-10-31 21:42:00 -0300

    Adding environment variable to mongodb

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const http = require('http')
 const app = require('./config/express')();
 const database = require('./config/database');
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/bea-project';
 
-database('mongodb://localhost/bea-project');
+database(MONGODB_URI);
 
 http.createServer(app).listen(app.get('port'), () => {
     console.log('Executando')
